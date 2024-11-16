e372d3b6629f7fb78fa8279b8f454a5440c8539b
Anthony Herrera <ankehecr100@gmail.com>
2024-11-16 16:11:23 -0500

    Initial commit with existing code

---
+++ src/products/products.controller.ts
@@ -0,0 +1,84 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Inject,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError } from 'rxjs';
+import { PaginationDto } from 'src/common';
+import { PRODUCT_SERVICE } from 'src/config';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+
+@Controller('products')
+export class ProductsController {
+  constructor(
+    @Inject(PRODUCT_SERVICE) private readonly productsClient: ClientProxy,
+  ) {}
+
+  @Post()
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productsClient.send(
+      { cmd: 'create_product' },
+      createProductDto,
+    );
+  }
+
+  @Get()
+  findAllProducts(@Query() paginationDto: PaginationDto) {
+    return this.productsClient.send(
+      {
+        cmd: 'find_all_products',
+      },
+      paginationDto,
+    );
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.productsClient
+      .send(
+        {
+          cmd: 'find_one_product',
+        },
+        { id: id },
+      )
+      .pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      );
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return this.productsClient
+      .send({ cmd: 'remove_product' }, { id: +id })
+      .pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      );
+  }
+
+  @Patch(':id')
+  patchProduct(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productsClient
+      .send({ cmd: 'update_product' }, { id: +id, ...updateProductDto })
+      .pipe(
+        catchError((err) => {
+          throw new RpcException(err);
+        }),
+      );
+  }
+}
